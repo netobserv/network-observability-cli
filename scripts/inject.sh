3e5e36da98ba1dd8f84522577a80a8b4488294dd
Julien Pinsonneau <jpinsonn@redhat.com>
2024-05-02 10:28:36 +0200

    Addressed feedback

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -6,21 +6,21 @@ if [ -z "$IMAGE" ]; then
   echo "image not provided, keeping current one"
 else 
   echo "updating CLI images to $IMAGE"
-  sed -i'' -e "/img=/c\img=\"$IMAGE\"" ./tmp/netobserv
+  sed -i.bak -e "/img=/c\img=\"$IMAGE\"" ./tmp/netobserv
 fi
 
 if [ -z "$PULL_POLICY" ]; then
   echo "pull policy not provided, keeping current one"
 else 
   echo "updating CLI pull policy to $PULL_POLICY"
-  sed -i'' -e "/  --i''mage-pull-policy/c\  --i''mage-pull-policy='$PULL_POLICY' \\\\" ./tmp/netobserv
+  sed -i.bak -e "/  --i.bakmage-pull-policy/c\  --i.bakmage-pull-policy='$PULL_POLICY' \\\\" ./tmp/netobserv
 fi
 
 if [ -z "$VERSION" ]; then
   echo "version not provided, keeping current one"
 else 
   echo "updating CLI version to $VERSION"
-  sed -i'' -e "/version=/c\version=\"$VERSION\"" ./tmp/netobserv
+  sed -i.bak -e "/version=/c\version=\"$VERSION\"" ./tmp/netobserv
 fi
 
 prefix=
@@ -31,9 +31,9 @@ if [ -z "$KREW_PLUGIN" ] || [ "$KREW_PLUGIN" = "false" ]; then
   fi 
   echo "updating K8S CLI to $K8S_CLI_BIN"
   # remove unecessary call
-  sed -i'' -e "/K8S_CLI_BIN_PATH=/d" ./tmp/functions.sh
+  sed -i.bak -e "/K8S_CLI_BIN_PATH=/d" ./tmp/functions.sh
   # replace only first match to force default
-  sed -i'' -e "0,/K8S_CLI_BIN=/c\K8S_CLI_BIN=$K8S_CLI_BIN" ./tmp/functions.sh
+  sed -i.bak -e "0,/K8S_CLI_BIN=/c\K8S_CLI_BIN=$K8S_CLI_BIN" ./tmp/functions.sh
   # prefix with oc / kubectl for local install
   prefix="$K8S_CLI_BIN-"
   echo "prefixing with $prefix"
@@ -41,24 +41,25 @@ if [ -z "$KREW_PLUGIN" ] || [ "$KREW_PLUGIN" = "false" ]; then
 fi
 
 # inject YAML files to functions.sh
-sed -i'' -e '/namespaceYAMLContent/{r ./res/namespace.yml' -e 'd}' ./tmp/functions.sh
-sed -i'' -e '/saYAMLContent/{r ./res/service-account.yml' -e 'd}' ./tmp/functions.sh
-sed -i'' -e '/flowAgentYAMLContent/{r ./res/flow-capture.yml' -e 'd}' ./tmp/functions.sh
-sed -i'' -e '/packetAgentYAMLContent/{r ./res/packet-capture.yml' -e 'd}' ./tmp/functions.sh
-sed -i'' -e '/collectorServiceYAMLContent/{r ./res/collector-service.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak -e '/namespaceYAMLContent/{r ./res/namespace.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak -e '/saYAMLContent/{r ./res/service-account.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak -e '/flowAgentYAMLContent/{r ./res/flow-capture.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak -e '/packetAgentYAMLContent/{r ./res/packet-capture.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak -e '/collectorServiceYAMLContent/{r ./res/collector-service.yml' -e 'd}' ./tmp/functions.sh
 
 # inject updated functions to commands
-sed -i'' -e '/source.*/{r ./tmp/functions.sh' -e 'd}' ./tmp/"$prefix"netobserv
+sed -i.bak -e '/source.*/{r ./tmp/functions.sh' -e 'd}' ./tmp/"$prefix"netobserv
 
 if [ -z "$3" ]; then
   echo "pull policy not provided, keeping current ones"
 else 
   echo "updating CLI pull policy to $3"
-  sed -i'' -e "/  --i''mage-pull-policy/c\  --i''mage-pull-policy='$3' \\\\" ./tmp/oc-netobserv-flows
-  sed -i'' -e "/  --i''mage-pull-policy/c\  --i''mage-pull-policy='$3' \\\\" ./tmp/oc-netobserv-packets
+  sed -i.bak -e "/  --i.bakmage-pull-policy/c\  --i.bakmage-pull-policy='$3' \\\\" ./tmp/oc-netobserv-flows
+  sed -i.bak -e "/  --i.bakmage-pull-policy/c\  --i.bakmage-pull-policy='$3' \\\\" ./tmp/oc-netobserv-packets
 fi
 
 rm ./tmp/functions.sh
+rm ./tmp/*.bak
 
 if [ -z "$DIST_DIR" ]; then
   echo "output generated in tmp folder"
