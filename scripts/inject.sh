c670c02331f98e40948a0afc7499a86f5d23394b
Julien Pinsonneau <91894519+jpinsonneau@users.noreply.github.com>
2024-05-06 11:49:16 +0200

    NETOBSERV-1623 update sed commands to support macos (#37)
    * update sed commands to support macos
    
    * Addressed feedback
    
    * Changes to also work on MacOS
    
    ---------
    
    Co-authored-by: Steven Lee <stlee@redhat.com>

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -4,23 +4,23 @@ cp ./scripts/functions.sh ./tmp/functions.sh
 
 if [ -z "$IMAGE" ]; then
   echo "image not provided, keeping current one"
-else 
+else
   echo "updating CLI images to $IMAGE"
-  sed -i "/img=/c\img=\"$IMAGE\"" ./tmp/netobserv
+  sed -i.bak "s|^img=.*|img=\"$IMAGE\"|" ./tmp/netobserv
 fi
 
 if [ -z "$PULL_POLICY" ]; then
   echo "pull policy not provided, keeping current one"
-else 
+else
   echo "updating CLI pull policy to $PULL_POLICY"
-  sed -i "/  --image-pull-policy/c\  --image-pull-policy='$PULL_POLICY' \\\\" ./tmp/netobserv
+  sed -i.bak "s/--image-pull-policy=.*/--image-pull-policy='$PULL_POLICY' \\\\/" ./tmp/netobserv
 fi
 
 if [ -z "$VERSION" ]; then
   echo "version not provided, keeping current one"
-else 
+else
   echo "updating CLI version to $VERSION"
-  sed -i "/version=/c\version=\"$VERSION\"" ./tmp/netobserv
+  sed -i.bak "s/^version=.*/version=\"$VERSION\"/" ./tmp/netobserv
 fi
 
 prefix=
@@ -28,12 +28,12 @@ if [ -z "$KREW_PLUGIN" ] || [ "$KREW_PLUGIN" = "false" ]; then
   if [ -z "$K8S_CLI_BIN" ]; then
     echo "ERROR: K8S CLI not provided"
     exit 1
-  fi 
+  fi
   echo "updating K8S CLI to $K8S_CLI_BIN"
-  # remove unecessary call
-  sed -i "/K8S_CLI_BIN_PATH=/d" ./tmp/functions.sh
+  # remove beginning lines
+  sed -i.bak '1,/K8S_CLI_BIN_PATH=/d' ./tmp/functions.sh
   # replace only first match to force default
-  sed -i "0,/K8S_CLI_BIN=/c\K8S_CLI_BIN=$K8S_CLI_BIN" ./tmp/functions.sh
+  sed -i.bak "s/^K8S_CLI_BIN=.*/K8S_CLI_BIN=$K8S_CLI_BIN/" ./tmp/functions.sh
   # prefix with oc / kubectl for local install
   prefix="$K8S_CLI_BIN-"
   echo "prefixing with $prefix"
@@ -41,28 +41,41 @@ if [ -z "$KREW_PLUGIN" ] || [ "$KREW_PLUGIN" = "false" ]; then
 fi
 
 # inject YAML files to functions.sh
-sed -i -e '/namespaceYAMLContent/{r ./res/namespace.yml' -e 'd}' ./tmp/functions.sh
-sed -i -e '/saYAMLContent/{r ./res/service-account.yml' -e 'd}' ./tmp/functions.sh
-sed -i -e '/flowAgentYAMLContent/{r ./res/flow-capture.yml' -e 'd}' ./tmp/functions.sh
-sed -i -e '/packetAgentYAMLContent/{r ./res/packet-capture.yml' -e 'd}' ./tmp/functions.sh
-sed -i -e '/collectorServiceYAMLContent/{r ./res/collector-service.yml' -e 'd}' ./tmp/functions.sh
+sed -i.bak '/namespaceYAMLContent/{r ./res/namespace.yml
+d
+}' ./tmp/functions.sh
+sed -i.bak '/saYAMLContent/{r ./res/service-account.yml
+d
+}' ./tmp/functions.sh
+sed -i.bak '/flowAgentYAMLContent/{r ./res/flow-capture.yml
+d
+}' ./tmp/functions.sh
+sed -i.bak '/packetAgentYAMLContent/{r ./res/packet-capture.yml
+d
+}' ./tmp/functions.sh
+sed -i.bak '/collectorServiceYAMLContent/{r ./res/collector-service.yml
+d
+}' ./tmp/functions.sh
 
 # inject updated functions to commands
-sed -i -e '/source.*/{r ./tmp/functions.sh' -e 'd}' ./tmp/"$prefix"netobserv
+sed -i.bak '/source.*/{r ./tmp/functions.sh
+d
+}' ./tmp/"$prefix"netobserv
 
 if [ -z "$3" ]; then
   echo "pull policy not provided, keeping current ones"
-else 
+else
   echo "updating CLI pull policy to $3"
-  sed -i "/  --image-pull-policy/c\  --image-pull-policy='$3' \\\\" ./tmp/oc-netobserv-flows
-  sed -i "/  --image-pull-policy/c\  --image-pull-policy='$3' \\\\" ./tmp/oc-netobserv-packets
+  sed -i.bak "s/--image-pull-policy=.*/--image-pull-policy='$3' \\\\/" ./tmp/oc-netobserv-flows
+  sed -i.bak "s/--image-pull-policy=.*/--image-pull-policy='$3' \\\\/" ./tmp/oc-netobserv-packets
 fi
 
 rm ./tmp/functions.sh
+rm ./tmp/*.bak
 
 if [ -z "$DIST_DIR" ]; then
   echo "output generated in tmp folder"
-else 
+else
   echo "output generated in $DIST_DIR folder"
   cp -a ./tmp/. ./"$DIST_DIR"
   rm -rf ./tmp
